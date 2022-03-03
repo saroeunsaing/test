b0bc39858159a6703c844e20a75c3f3596cb48d4
GURUTECH <guru.tech.kh@gmail.com>
2022-03-03 09:33:50 +0700

    fjldksfj

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 291);
+            this.ClientSize = new System.Drawing.Size(633, 380);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
